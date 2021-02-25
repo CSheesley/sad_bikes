c951c34e9d60a636f59e3106183d2e9f0ced4527
Corey Sheesley <corey.sheesley@gmail.com>
2021-02-24 18:20:46 -0700

    Configure and create search factories

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Search, type: :model do
     it { should validate_presence_of :response_json }
   end
 
+
+  it 'has a working factories' do
+    expect {
+      search1 = create(:search, :no_incidents)
+      search2 = create(:search, :one_incident)
+      search3 = create(:search, :some_incidents)
+      search4 = create(:search, :many_incidents)
+    }.to change{ Search.count }.by(4)
+  end
+
 end

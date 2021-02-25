3f38b8df10e98429e92578d14734f392da972943
Corey Sheesley <corey.sheesley@gmail.com>
2021-02-24 21:46:32 -0700

    Find or create a Search based on :zipcode - add :response_json next

--- app/controllers/api/v1/incidents_controller.rb
+++ app/controllers/api/v1/incidents_controller.rb
@@ -1,6 +1,26 @@
 class Api::V1::IncidentsController < ApplicationController
 
   def index
+    search = find_or_create_search
+    #filtered  = Service::SearchFilter(search).results
+    #incidents = IncidentsSerializer(search).results
+
+    # render status: :200, json: incidents
   end
 
+  private
+
+  def search_params
+    params.permit(:zipcode)
+  end
+
+  def find_or_create_search
+    criteria = {
+      params: { zipcode: search_params[:zipcode].to_i }
+    }
+
+    Search.find_or_create_by(criteria) do |search|
+      search.response_json = BikeWiseService.new(search_params).to_json
+    end
+  end
 end
--- spec/requests/api/v1/get_incidents_request_spec.rb
+++ spec/requests/api/v1/get_incidents_request_spec.rb
@@ -9,40 +9,58 @@ RSpec.describe Api::V1::IncidentsController, type: :request do
         get '/api/v1/incidents', params: { zipcode: 80403 }
 
         parsed = JSON.parse(response.body)
-        expected_json= {
-          "incidents": [
+        expected_body = {
+          incidents: [
             {
               id: 76603,
+              type: "Theft",
               title: "Stolen 2015 Giant Escape 1(black)",
               description: "",
               address: "Denver, CO, 80401",
               occurred_at: Time.at(1502690400).strftime('%m-%d-%Y %H:%M %Z'),
               url: "https://bikewise.org/api/v1/incidents/76603",
-              image_url: "https://files.bikeindex.org/uploads/Pu/95863/large_IMG_6792.JPG",
-              type: "Theft",
+              image_url: "https://files.bikeindex.org/uploads/Pu/95863/large_IMG_6792.JPG"
             }
           ]
         }
 
         expect(response).to have_http_status(200)
-        expect(parsed).to eq(expected_json)
+        expect(parsed).to eq(expected_body)
       end
 
       it 'can sorts the results from newest to oldest by default' do
+        # get '/api/v1/incidents', params: { zipcode:  }
       end
 
       it 'can sort the results from oldest to newest' do
+        # get '/api/v1/incidents', params: { zipcode: xxxxx, sort: 'desc' }
       end
 
       it 'can filter results based on the incident type' do
+        # get '/api/v1/incidents', params: { zipcode: xxxxx, type: 'hazard' }
         # hazard, theft - use :some_incidents_json
       end
 
       context 'creating Search objects' do
         it 'creates a new Search object - with a :response_json attribute' do
+          expect {
+            get '/api/v1/incidents', params: { zipcode: 80401 }
+          }.to change { Search.count }.by(1)
+
+          search = Search.last
+          json = file_fixture('no_incidents.json').read.rstrip
+
+          expect(search.params[:zipcode]).to eq(80401)
+          expect(search.response_json).to eq(json)
         end
 
         it 'does not create a new Search object if an existing object with the same :zipcode was recently created (last 2 days)' do
+          search = create(:search, :one_incident)
+
+          expect {
+            get '/api/v1/incidents', params: { zipcode: 80401 }
+          }.to_not change { Search.count }
+
         end
       end
     end
